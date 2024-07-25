05c0dbe77770257cbf59bab9cdeff6548f430b08
Anton <web-gambit@mail.ru>
2024-07-25 23:36:03 +0500

    Use API client

--- go.mod
+++ go.mod
@@ -7,4 +7,9 @@ require (
 	github.com/joho/godotenv v1.5.1
 )
 
-require github.com/gocarina/gocsv v0.0.0-20240520201108-78e41c74b4b1
+require (
+	github.com/gocarina/gocsv v0.0.0-20240520201108-78e41c74b4b1
+	github.com/nicklaw5/helix/v2 v2.30.0
+)
+
+require github.com/golang-jwt/jwt/v4 v4.0.0 // indirect
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"io/fs"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"slices"
@@ -13,21 +14,64 @@ import (
 	"github.com/gempir/go-twitch-irc/v4"
 	"github.com/gocarina/gocsv"
 	"github.com/joho/godotenv"
+	"github.com/nicklaw5/helix/v2"
 )
 
 const rootDir = "channels"
+
+var apiClient *helix.Client
 
 type user struct {
 	Name     string    `csv:"name"`
 	LastSeen time.Time `csv:"last_seen"`
 }
 
-func getUsers(client *twitch.Client, channelName string) []string {
+func getUserNames(client *twitch.Client, channelName string) ([]string, error) {
 	userNames, err := client.Userlist(channelName)
 	if err != nil {
 		log.Printf("Error getting User list of %s", channelName)
+		return nil, err
 	}
-	return userNames
+	return userNames, nil
+}
+
+func getUsersInfo(names ...string) ([]helix.User, error) {
+	resp, err := apiClient.GetUsers(&helix.UsersParams{
+		Logins: names,
+	})
+
+	if err != nil {
+		log.Print("Error getting users info")
+		return nil, err
+	}
+
+	return resp.Data.Users, nil
+}
+
+func getVipNames(channelName string) ([]string, error) {
+	usersInfo, err := getUsersInfo(channelName)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := apiClient.GetChannelVips(&helix.GetChannelVipsParams{
+		BroadcasterID: usersInfo[0].ID,
+		First:         100,
+	})
+	if err != nil || resp.StatusCode != http.StatusOK {
+		log.Printf("Error getting VIPs of %s", channelName)
+		if err == nil {
+			err = errors.New(resp.ErrorMessage)
+		}
+		return nil, err
+	}
+
+	vips := make([]string, 0, len(resp.Data.ChannelsVips))
+	for _, vip := range resp.Data.ChannelsVips {
+		vips = append(vips, vip.UserLogin)
+	}
+
+	return vips, nil
 }
 
 func updateUsersFile(channelName string, userNames []string) {
@@ -52,7 +96,9 @@ func updateUsersFile(channelName string, userNames []string) {
 		}
 		// filter online users
 		for _, user := range usersFromFile {
-			if slices.Contains(userNames, user.Name) { continue }
+			if slices.Contains(userNames, user.Name) {
+				continue
+			}
 			users = append(users, user)
 		}
 	}
@@ -66,7 +112,7 @@ func updateUsersFile(channelName string, userNames []string) {
 		return a.LastSeen.Compare(b.LastSeen)
 	})
 
-	if _, err = f.Seek(0,0); err != nil {
+	if _, err = f.Seek(0, 0); err != nil {
 		log.Fatal(err)
 	}
 	if err = gocsv.MarshalFile(&users, f); err != nil {
@@ -78,8 +124,26 @@ func updateUsersFile(channelName string, userNames []string) {
 func updateUsers(client *twitch.Client, channelName string) {
 	for {
 		time.Sleep(5 * time.Minute)
-		userNames := getUsers(client, channelName)
-		updateUsersFile(channelName, userNames)
+		userNames, err := getUserNames(client, channelName)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+
+		vipNames, err := getVipNames(channelName)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+
+		presentVipNames := make([]string, 0, len(vipNames))
+		for _, vipName := range vipNames {
+			if slices.Contains(userNames, vipName) {
+				presentVipNames = append(presentVipNames, vipName)
+			}
+		}
+
+		updateUsersFile(channelName, presentVipNames)
 	}
 }
 
@@ -98,28 +162,36 @@ func main() {
 
 	mkDir(rootDir)
 
-	nick := os.Getenv("NICK")
-	pass := os.Getenv("PASS")
+	nick := os.Getenv("SA_NICK")
+	pass := os.Getenv("SA_PASS")
 
-	client := twitch.NewClient(nick, pass)
-	client.Capabilities = append(client.Capabilities, twitch.MembershipCapability)
+	ircClient := twitch.NewClient(nick, pass)
+	ircClient.Capabilities = append(ircClient.Capabilities, twitch.MembershipCapability)
 
-	client.OnSelfJoinMessage(func(message twitch.UserJoinMessage) {
+	ircClient.OnSelfJoinMessage(func(message twitch.UserJoinMessage) {
 		go func() {
 			channelName := message.Channel
 			log.Printf("Joined %s", channelName)
-			updateUsers(client, channelName)
+			updateUsers(ircClient, channelName)
 		}()
 	})
 
-	// client.OnPrivateMessage(func(message twitch.PrivateMessage) {
-	// 	client.Say(message.Channel, "hey")
+	// ircClient.OnPrivateMessage(func(message twitch.PrivateMessage) {
+	// 	ircClient.Say(message.Channel, "hey")
 	// })
 
-	channels := strings.Split(os.Getenv("CHANNELS"), ",")
-	client.Join(channels...)
+	channels := strings.Split(os.Getenv("SA_CHANNELS"), ",")
+	ircClient.Join(channels...)
 
-	err = client.Connect()
+	apiClient, err = helix.NewClient(&helix.Options{
+		ClientID:        "jmaoofuyr1c4v8lqzdejzfppdj5zym",
+		UserAccessToken: os.Getenv("SA_USER_ACCESS_TOKEN"),
+	})
+	if err != nil {
+		log.Fatal("Error creating API client")
+	}
+
+	err = ircClient.Connect()
 	if err != nil {
 		log.Fatal("Error connecting to Twitch")
 	}
